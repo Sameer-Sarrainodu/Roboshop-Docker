fc62189cf2b22d9ac909b5ac12cac1bf4ac5d5e0
Sameer-Sarrainodu <sameer339977.com>
2025-10-23 12:18:09 +0530

    docker -roboshop

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -f /tmp/mysql_root_password.txt]; then
-    PASSWORD=$(cat /tmp/mysql_root_password.txt)
-    echo "Accessed Root password"
+if [ -f /tmp/mysql_root_password.txt ]; then
+  PASSWORD=$(cat /tmp/mysql_root_password.txt)
+  export MYSQL_ROOT_PASSWORD=$PASSWORD
 else
-    echo "password file not found"
-    exit 1
+  echo "password file not found"
+  exit 1
 fi
 
+exec mysqld
 export MYSQL_ROOT_PASSWORD="$PASSWORD"
 rm -rf /tmp/mysql_root_password.txt
 exec /entrypoint.sh mysqld
