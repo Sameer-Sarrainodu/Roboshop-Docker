2ec093c44afc58f426bb33b99a63d83f3f3e0e80
Sameer-Sarrainodu <sameer339977.com>
2025-10-23 12:48:46 +0530

    roboshop

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Check if secret file exists
 if [ -f /tmp/mysql_root_password.txt ]; then
     PASSWORD=$(cat /tmp/mysql_root_password.txt)
-    echo "Accessed root password"
-    export MYSQL_ROOT_PASSWORD="$PASSWORD"
-    # Cleanup after reading
-    rm -rf /tmp/mysql_root_password.txt
+    echo "Accessed Root password"
 else
-    echo "password file not found"
+    echo "Password file not found"
     exit 1
 fi
-
-# Start MySQL
-exec mysqld
+# Making it as available in env
+export MYSQL_ROOT_PASSWORD=$PASSWORD
+rm -rf /tmp/mysql_root_password.txt
+exec /entrypoint.sh mysqld
